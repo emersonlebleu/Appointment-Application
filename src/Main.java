216e7d88a7f6e8433375cb5977e817977ab26f5b
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-14 17:03:13 -0600

    Add main page and basic view.

--- src/Main.java
+++ src/Main.java
@@ -30,7 +30,7 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) {
-        Locale.setDefault(new Locale("fr"));
+//        Locale.setDefault(new Locale("fr"));
 
         JDBC.connect();
         launch();
