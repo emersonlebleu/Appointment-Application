90d95f040ad7c75eae8f8b530b59f6d4786a104d
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-17 08:14:01 -0600

    Add methods.

--- src/model/Country.java
+++ src/model/Country.java
@@ -1,11 +1,29 @@
 package model;
 
+/** Class to hold information regarding country. */
 public class Country {
     private int countryId;
     private String countryName;
 
+    /** Constructor */
     public Country(int countryId, String countryName) {
         this.countryId = countryId;
+        this.countryName = countryName;
+    }
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
 }
