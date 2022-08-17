fbbff9a3b43f34c51fbf597021e322e9fff95794
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-17 08:16:00 -0600

    Finish javadocs.

--- src/model/Country.java
+++ src/model/Country.java
@@ -10,19 +10,23 @@ public class Country {
         this.countryId = countryId;
         this.countryName = countryName;
     }
-
+    /** Get the id of the country.
+     * @return the id of the country */
     public int getCountryId() {
         return countryId;
     }
-
+    /** Set the id of the country.
+     * @param countryId the id of the country. */
     public void setCountryId(int countryId) {
         this.countryId = countryId;
     }
-
+    /** Get the name of the country.
+     * @return the name of the country */
     public String getCountryName() {
         return countryName;
     }
-
+    /** Set the name of the country.
+     * @param countryName the name of the country. */
     public void setCountryName(String countryName) {
         this.countryName = countryName;
     }
