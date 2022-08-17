dec2b3434bc02c944c62adbea67279f4ae615e8f
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-17 13:28:44 -0600

    refactor properties.

--- src/model/Country.java
+++ src/model/Country.java
@@ -2,32 +2,32 @@ package model;
 
 /** Class to hold information regarding country. */
 public class Country {
-    private int countryId;
-    private String countryName;
+    private int id;
+    private String name;
 
     /** Constructor */
     public Country(int countryId, String countryName) {
-        this.countryId = countryId;
-        this.countryName = countryName;
+        this.id = countryId;
+        this.name = countryName;
     }
     /** Get the id of the country.
      * @return the id of the country */
-    public int getCountryId() {
-        return countryId;
+    public int getId() {
+        return id;
     }
     /** Set the id of the country.
-     * @param countryId the id of the country. */
-    public void setCountryId(int countryId) {
-        this.countryId = countryId;
+     * @param id the id of the country. */
+    public void setId(int id) {
+        this.id = id;
     }
     /** Get the name of the country.
      * @return the name of the country */
-    public String getCountryName() {
-        return countryName;
+    public String getName() {
+        return name;
     }
     /** Set the name of the country.
-     * @param countryName the name of the country. */
-    public void setCountryName(String countryName) {
-        this.countryName = countryName;
+     * @param name the name of the country. */
+    public void setName(String name) {
+        this.name = name;
     }
 }
