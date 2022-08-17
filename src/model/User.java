cace7e051200fb73f886ba913d4dc6d64b88837e
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-17 14:07:56 -0600

    Finish docs

--- src/model/User.java
+++ src/model/User.java
@@ -11,27 +11,33 @@ public class User {
         this.name = name;
         this.password = password;
     }
-
+    /** Get the id of the user.
+     * @return the id of the user */
     public int getId() {
         return id;
     }
-
+    /** Set the id of the user.
+     * @param id the id of the user. */
     public void setId(int id) {
         this.id = id;
     }
-
+    /** Get the name of the user.
+     * @return the name of the user */
     public String getName() {
         return name;
     }
-
+    /** Set the name of the user.
+     * @param name the name of the user. */
     public void setName(String name) {
         this.name = name;
     }
-
+    /** Get the password of the user.
+     * @return the password of the user */
     public String getPassword() {
         return password;
     }
-
+    /** Set the password of the user.
+     * @param password the password of the user. */
     public void setPassword(String password) {
         this.password = password;
     }
