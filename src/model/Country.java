edda54c699d27b0d8d3830d04474159489739af2
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-17 08:08:18 -0600

    Create properties and contstructor.

--- src/model/Country.java
+++ src/model/Country.java
@@ -1,4 +1,11 @@
 package model;
 
 public class Country {
+    private int countryId;
+    private String countryName;
+
+    public Country(int countryId, String countryName) {
+        this.countryId = countryId;
+        this.countryName = countryName;
+    }
 }
