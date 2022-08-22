461072fb8e333193e378ce0486fafadbafa5ca82
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-22 14:43:14 -0600

    Add reports basic structure.

--- out/production/ScheduleApp/README.txt
+++ out/production/ScheduleApp/README.txt
@@ -7,6 +7,6 @@ DATE: 22AUG2022
 IDE: IntelliJ Community Edition 2022.2
 JDK: jdk-17.0.4
 JavaFX: javafx-sdk-18.0.2
-RUN BY:
-ADDITIONAL REPORT (A3F):
+RUN BY: 1) Launch Application 2) login with either username: test password: test OR username: admin password: admin 3) Use buttons to navigate to the appropriate views of interest for your use case.
+ADDITIONAL REPORT (A3F): Display total appointments for current logged-in user.
 MYSQL DRIVER: mysql-connector-java-8.0.30
--- src/controller/Reports.java
+++ src/controller/Reports.java
@@ -1,13 +1,27 @@
 package controller;
 
 import javafx.fxml.Initializable;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Reports implements Initializable {
+    public Tab scheduleCurrUserTab;
+    public Tab numApptTab;
+    public Tab scheduleContactTab;
+    public TableView totalBType;
+    public TableView totalBMonth;
+    public TableView totalBTyMo;
+    public TableColumn tbtTypeCol;
+    public TableColumn tbtApptCol;
+    public TableColumn tbmMonthCol;
+    public TableColumn tbmApptCol;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        
     }
 }
