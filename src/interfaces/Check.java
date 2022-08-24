eaaf7fa38fc04c840468fe666c44adc92f3053ba
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-24 16:50:45 -0600

    Update check interface docs.

--- src/interfaces/Check.java
+++ src/interfaces/Check.java
@@ -1,5 +1,6 @@
 package interfaces;
 
+/** Functional interface for the alert functionality. */
 @FunctionalInterface
 public interface Check {
     void popUp(Type type, String title, String message);
