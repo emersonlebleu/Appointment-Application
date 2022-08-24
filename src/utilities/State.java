91ca5c2948760b33ba9faec940b1669773004e8f
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-24 17:17:13 -0600

    Finish state docs.

--- src/utilities/State.java
+++ src/utilities/State.java
@@ -1,5 +1,7 @@
 package utilities;
 
+/** A utility class for state of application. */
 public abstract class State {
+    /** A property indicating which view should be in front. */
     public static String infront;
 }
