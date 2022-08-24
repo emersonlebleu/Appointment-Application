62463e62108b9b9d1a1fe88f57caecb96e133fdf
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-24 17:04:39 -0600

    Update Report docs.

--- src/model/Report.java
+++ src/model/Report.java
@@ -1,26 +1,29 @@
 package model;
 
-import java.time.LocalDateTime;
 
 public class Report {
     private Integer month;
     private String type;
     private Integer numberAppt;
 
+    /** Report constructor instance if there is a month type and number */
     public Report(Integer month, String type, Integer numberAppt){
         this.month = month;
         this.type = type;
         this.numberAppt = numberAppt;
     }
+    /** Report constructor instance if there is a month and number */
     public Report(Integer month, Integer numberAppt){
         this.month = month;
         this.numberAppt = numberAppt;
     }
+    /** Report constructor instance if there is a type and number */
     public Report(String type, Integer numberAppt){
         this.type = type;
         this.numberAppt = numberAppt;
     }
-
+    /** Months returned from DB are numerical. When the controller accesses a month for the row in report they get the
+     * corresponding alphabetical representation. */
     public String getMonth(){
         String monthText = null;
         switch (this.month){
@@ -63,11 +66,13 @@ public class Report {
         }
         return monthText;
     }
-
+    /** Used by reports to get the number of appointments.
+     * @return the number of appointments for that report. */
     public Integer getNumberAppt() {
         return numberAppt;
     }
-
+    /** Used by reports controller to get the type.
+     * @return the type of appointment for that report row. */
     public String getType() {
         return type;
     }
