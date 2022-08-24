b3c7dd9cba13dcf3ad68601eaf798928dc2c4db7
Emerson Lebleu <emerson.lebleu@gmail.com>
2022-08-24 16:52:49 -0600

    Update appointment docs.

--- src/model/Appointment.java
+++ src/model/Appointment.java
@@ -1,16 +1,9 @@
 package model;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.stage.StageStyle;
 import utilities.CurrentSession;
-
-import java.sql.Date;
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Optional;
 
 /** Appointment Class for appointment object. */
 public class Appointment {
@@ -43,6 +36,7 @@ public class Appointment {
         this.contact = contactId;
 
     }
+    /** New appointment method. Generate new appointment without an appointment ID. */
     public Appointment(String title, String description, String location, String type, LocalDateTime start, LocalDateTime end, Integer customerId, Integer userId, Integer contactId){
         this.title = title;
         this.description = description;
@@ -176,12 +170,14 @@ public class Appointment {
     public void setContact(Integer id){
         this.contact = id;
     }
-
+    /** Get the formatted start date & time.
+     * @return formatted start date & time. */
     public String getStartFormat() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MM/dd/yyyy HH:mm zzz");
         return startFormat.format(formatter);
     }
-
+    /** Get the formatted end date & time.
+     * @return formatted end date & time. */
     public String getEndFormat() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MM/dd/yyyy HH:mm zzz");
         return endFormat.format(formatter);
